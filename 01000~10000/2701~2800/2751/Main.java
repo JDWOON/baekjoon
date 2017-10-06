0e24b3b08b8f07d297e2ee806a9f52fdf01e8af2
jdown <jda21@naver.com>
2017-10-06 18:05:09 +0900

    some sort problems

---
+++ 01000~10000/2701~2800/2751/Main.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int[] p = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			p[i] = sc.nextInt();
+		}
+
+		Arrays.sort(p);
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(p[i]);
+		}
+	}
+}
