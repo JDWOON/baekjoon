1a72c0a3b51d383720cca31df5f3d27e3b6a8095
jdown <jda21@naver.com>
2017-12-02 20:04:51 +0900

    no.2751 resolve with Merge Sort

--- 01000~10000/2701~2800/2751/Main.java
+++ 01000~10000/2701~2800/2751/Main.java
@@ -1,6 +1,5 @@
 package baekjoon;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -15,7 +14,38 @@ public class Main {
 			p[i] = sc.nextInt();
 		}
 
-		Arrays.sort(p);
+		int[] result = new int[n];
+
+		for (int len = 1; len < n; len *= 2) {
+			for (int left = 0; left < n; left += len * 2) {
+				int right = left + len;
+				int end = Math.min(n, right + len);
+				int a = left;
+				int b = right;
+
+				for (int pos = left; pos < end; pos++) {
+					if (a < right && b < end) {
+						if (p[a] < p[b]) {
+							result[pos] = p[a];
+							a++;
+						} else {
+							result[pos] = p[b];
+							b++;
+						}
+					} else if (a < right) {
+						result[pos] = p[a];
+						a++;
+					} else if (b < end) {
+						result[pos] = p[b];
+						b++;
+					}
+				}
+			}
+
+			for (int i = 0; i < n; i++) {
+				p[i] = result[i];
+			}
+		}
 
 		for (int i = 0; i < n; i++) {
 			System.out.println(p[i]);
