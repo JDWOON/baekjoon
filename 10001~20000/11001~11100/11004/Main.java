0138bf183b28725142f3b9ebbb758ea5e23a45f6
jdown <jda21@naver.com>
2017-12-28 16:55:03 +0900

    no.11004

---
+++ 10001~20000/11001~11100/11004/Main.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] p = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			p[i] = sc.nextInt();
+		}
+
+		Arrays.sort(p);
+
+		System.out.println(p[k - 1]);
+	}
+}
