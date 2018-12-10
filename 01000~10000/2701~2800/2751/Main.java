6ac364d475bdf948941a782d93c8c30d8edcf478
jdown <jda21@naver.com>
2018-12-10 17:46:33 +0900

    no.2751

--- 01000~10000/2701~2800/2751/Main.java
+++ 01000~10000/2701~2800/2751/Main.java
@@ -1,54 +1,32 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = sc.nextInt();
+		int n = Integer.parseInt(br.readLine());
 
 		int[] p = new int[n];
 
 		for (int i = 0; i < n; i++) {
-			p[i] = sc.nextInt();
+			p[i] = Integer.parseInt(br.readLine());
 		}
 
-		int[] result = new int[n];
+		Arrays.sort(p);
 
-		for (int len = 1; len < n; len *= 2) {
-			for (int left = 0; left < n; left += len * 2) {
-				int right = left + len;
-				int end = Math.min(n, right + len);
-				int a = left;
-				int b = right;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-				for (int pos = left; pos < end; pos++) {
-					if (a < right && b < end) {
-						if (p[a] < p[b]) {
-							result[pos] = p[a];
-							a++;
-						} else {
-							result[pos] = p[b];
-							b++;
-						}
-					} else if (a < right) {
-						result[pos] = p[a];
-						a++;
-					} else if (b < end) {
-						result[pos] = p[b];
-						b++;
-					}
-				}
-			}
-
-			for (int i = 0; i < n; i++) {
-				p[i] = result[i];
-			}
+		for (int i = 0; i < p.length; i++) {
+			bw.write(p[i] + "\n");
 		}
 
-		for (int i = 0; i < n; i++) {
-			System.out.println(p[i]);
-		}
+		bw.flush();
 	}
 }
