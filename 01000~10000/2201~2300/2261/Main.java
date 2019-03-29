29d5d51658557b40933c28097e6597fffd45110b
jdw <jda21@naver.com>
2019-03-29 16:47:47 +0900

    no.2261 divide and conquer

--- 01000~10000/2201~2300/2261/Main.java
+++ 01000~10000/2201~2300/2261/Main.java
@@ -1,40 +1,105 @@
-package baekjoon;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] p = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		Dot[] dots = new Dot[n];
 		for (int i = 0; i < n; i++) {
-			int x = sc.nextInt() + 10000;
-			int y = sc.nextInt() + 10000;
-			p[i] = x * 20001 + y;
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			dots[i] = new Dot(x, y);
 		}
 
-		Arrays.sort(p);
+		Arrays.sort(dots);
 
-		int min = (p[0] / 20001 - p[1] / 20001) * (p[0] / 20001 - p[1] / 20001);
-		min += (p[0] % 20001 - p[1] % 20001) * (p[0] % 20001 - p[1] % 20001);
-		for (int i = 0; i < p.length - 1; i++) {
-			int x1 = p[i] / 20001, y1 = p[i] % 20001;
-			for (int j = i + 1; j < p.length; j++) {
-				int x2 = p[j] / 20001, y2 = p[j] % 20001;
+		System.out.println(solve(dots, 0, n - 1));
+	}
 
-				int len = (x1 - x2) * (x1 - x2);
-				if (len >= min) {
+	public static int solve(Dot[] dots, int left, int right) {
+		if (right - left == 0) {
+			return Integer.MAX_VALUE;
+		} else if (right - left == 1) {
+			return getDist(dots[right], dots[left], 0);
+		} else {
+			int mid = (left + right) / 2;
+			int result = Math.min(solve(dots, left, mid), solve(dots, mid + 1, right));
+
+			int line = (dots[mid].x + dots[mid + 1].x) / 2;
+
+			ArrayList<Dot> list = new ArrayList<>();
+
+			for (int i = mid + 1; i <= right; i++) {
+				if (getDist(new Dot(line, 0), dots[i], 1) >= result) {
 					break;
-				} else {
-					len += (y1 - y2) * (y1 - y2);
-					if (len < min) {
-						min = len;
+				}
+				list.add(dots[i]);
+			}
+
+			for (int i = mid; i >= left; i--) {
+				if (getDist(new Dot(line, 0), dots[i], 1) >= result) {
+					break;
+				}
+
+				list.add(dots[i]);
+			}
+
+			if (!list.isEmpty()) {
+				list.sort((o1, o2) -> (o1.y < o2.y) ? -1 : (o1.y == o2.y) ? 0 : 1);
+
+				for (int i = 0; i < list.size(); ++i) {
+					Dot dotA = list.get(i);
+					for (int j = i + 1; j < list.size(); j++) {
+						Dot dotB = list.get(j);
+						if (getDist(dotA, dotB, 2) >= result) {
+							break;
+						}
+						result = Math.min(result, getDist(dotA, dotB, 0));
 					}
 				}
 			}
+
+			return result;
+		}
+	}
+
+	public static int getDist(Dot a, Dot b, int mode) {
+		if (mode == 0) {
+			return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+		} else if (mode == 1) {
+			return (a.x - b.x) * (a.x - b.x);
+		} else if (mode == 2) {
+			return (a.y - b.y) * (a.y - b.y);
+		} else {
+			return -1;
+		}
+	}
+
+	public static class Dot implements Comparable<Dot> {
+		int x, y;
+
+		Dot(int x, int y) {
+			this.x = x;
+			this.y = y;
 		}
 
-		System.out.println(min);
+		@Override
+		public int compareTo(Dot o) {
+			if (this.x == o.x) {
+				return Integer.compare(this.y, o.y);
+			} else {
+				return Integer.compare(this.x, o.x);
+			}
+		}
 	}
 }
