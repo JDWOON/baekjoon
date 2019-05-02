f1b5a0e3c8afb6aff442cf1c07b7ef264c7f8001
jdw <jda21@naver.com>
2019-05-02 14:50:41 +0900

    no.11004 fix

--- 10001~20000/11001~11100/11004/Main.java
+++ 10001~20000/11001~11100/11004/Main.java
@@ -1,11 +1,10 @@
-package baekjoon;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -26,9 +25,49 @@ public class Main {
 			p[i] = Integer.parseInt(st.nextToken());
 		}
 
-		Arrays.sort(p);
-
-		bw.write(p[k - 1] + "\n");
+		bw.write(solve(p, 0, n - 1, k - 1) + "\n");
 		bw.flush();
+	}
+
+	public static int solve(int[] list, int left, int right, int k) {
+		if (left == right) {
+			return list[left];
+		}
+
+		Random r = new Random();
+		int pivot = left + r.nextInt(right - left);
+		pivot = partition(list, left, right, pivot);
+
+		if (k == pivot) {
+			return list[k];
+		} else if (k < pivot) {
+			return solve(list, left, pivot - 1, k);
+		} else {
+			return solve(list, pivot + 1, right, k);
+		}
+	}
+
+	public static int partition(int[] list, int left, int right, int pivot) {
+		int val = list[pivot];
+
+		int temp = list[pivot];
+		list[pivot] = list[right];
+		list[right] = temp;
+
+		int now = left;
+		for (int i = left; i < right; i++) {
+			if (list[i] <= val) {
+				temp = list[i];
+				list[i] = list[now];
+				list[now] = temp;
+				now++;
+			}
+		}
+
+		temp = list[now];
+		list[now] = list[right];
+		list[right] = temp;
+
+		return now;
 	}
 }
