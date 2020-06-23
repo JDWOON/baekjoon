ddfe2002eacf43547951f42b53fceb54ebb32876
jdown21 <dawoon.jeong@bankwareglobal.com>
2020-06-23 17:51:29 +0900

    no.4354 edit

--- 01000~10000/4301~4400/4354/Main.java
+++ 01000~10000/4301~4400/4354/Main.java
@@ -1,5 +1,3 @@
-package baekjoon;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -8,36 +6,48 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class Main {
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		String s = br.readLine();
-		while (s.charAt(0) != '.') {
-			ArrayList<Integer> list = new ArrayList<Integer>();
-			for (int i = 1; i <= Math.sqrt(s.length()); i++) {
-				if (s.length() % i == 0) {
-					list.add(i);
-					list.add(s.length() / i);
-				}
-			}
-			for (int i = 0; i < s.length(); i++) {
-				for (int j = 0; j < list.size(); j++) {
-					if (s.charAt(i) != s.charAt(i % list.get(j))) {
-						list.remove(j);
-						j--;
-					}
-				}
-			}
-			int min = Integer.MAX_VALUE;
-			for (int i = 0; i < list.size(); i++) {
-				min = Math.min(min, list.get(i));
-			}
-			bw.write((s.length() / min) + "\n");
 
+		while (s.charAt(0) != '.') {
+			bw.write(solve(s) + "\n");
 			s = br.readLine();
 		}
 
 		bw.flush();
+
+	}
+
+	public static int solve(String s) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		for (int i = 1; i < s.length(); i++) {
+			if (s.length() % i == 0) {
+				list.add(i);
+			}
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			int nowLen = list.get(i);
+			boolean check = true;
+			for (int j = 0; check && j < s.length() / nowLen; j++) {
+				for (int k = 0; check && k < nowLen; k++) {
+					if (s.charAt(k) != s.charAt(j * nowLen + k)) {
+						check = false;
+						break;
+					}
+				}
+			}
+
+			if (check) {
+				return s.length() / nowLen;
+			}
+		}
+
+		return 1;
 	}
 }
